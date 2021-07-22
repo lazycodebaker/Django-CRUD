20bf20440b75d990c1bc8a4ee40197e1b3656f45
Anshuman Tiwari <lazycodebaker@outlook.com>
2021-07-23 05:22:07 +0530

    add-user

--- base/forms.py
+++ base/forms.py
@@ -1,3 +1,22 @@
 
-from django.forms import forms
+from django import forms
+from .models import User
 
+class UserRegister(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name','age','email','number','password']
+
+        widgets = {
+            'name' : forms.TextInput(attrs={'class':'form-control'}),
+            'email' : forms.EmailInput(attrs={'class':'form-control'}),
+            'age' : forms.NumberInput(attrs={'class':'form-control'}),
+            'number' : forms.NumberInput(attrs={'class':'form-control'}),
+            'password' : forms.TextInput(attrs={'class':'form-control'})
+        }
+
+
+
+      
+
+        
--- base/views.py
+++ base/views.py
@@ -1,6 +1,14 @@
+from base.models import User
 from django.shortcuts import render
+from .forms import UserRegister
 
 # Create your views here.
 
 def addAndshow(req):
-    return render(req,"addAndshow.html")
+    if req.method == "POST":
+        ur = UserRegister(req.POST)
+        ur.save()       
+    else:
+        ur = UserRegister()
+        
+    return render(req,"addAndshow.html",{"form":ur})
