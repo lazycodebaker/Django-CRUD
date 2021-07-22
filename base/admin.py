199be0672d984248e12515c2ce817517b157b2e6
Anshuman Tiwari <lazycodebaker@outlook.com>
2021-07-23 05:00:18 +0530

    crud

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
+from base.models import User
+
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name','age','email','number','password')
